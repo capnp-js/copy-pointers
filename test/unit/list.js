12085e56a8b7c2d00584fc798dadcbe0182aadcc
Tim Popham <popham@uw.edu>
2019-02-06 19:28:41 -0800

    Switch to Mocha and add Istanbul support

--- test/unit/list.js
+++ test/unit/list.js
@@ -1,27 +1,23 @@
 /* @flow */
 
-import test from "ava";
+import * as assert from "assert";
+import { describe, it } from "mocha";
 
 import { int32 } from "@capnp-js/read-data";
 
 import nonboolListTag from "../../src/nonboolListTag";
 
-test("`nonboolListTag`", t => {
-  t.plan(2);
-
-  const segment = {
-    id: 0,
-    raw: new Uint8Array(8),
-    end: 8,
-  };
-
+describe("nonboolListTag", function () {
+  const segment = { id: 0, raw: new Uint8Array(8), end: 8 };
   const object = {
     segment,
     position: 0,
   };
 
-  nonboolListTag(object, 11, {data: 288, pointers: 520});
+  it("decodes data section size and pointers section size from a list tag word", function () {
+    nonboolListTag(object, 11, {data: 288, pointers: 520});
 
-  t.is(int32(segment.raw, 0), (11<<2) | 0x00);
-  t.is(int32(segment.raw, 4), (520<<13) | (288>>>3));
+    assert.equal(int32(segment.raw, 0), (11<<2) | 0x00);
+    assert.equal(int32(segment.raw, 4), (520<<13) | (288>>>3));
+  });
 });
